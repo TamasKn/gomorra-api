5c0b1236fca86e855abc5613f13321580ae0112f
tx <tamas.knisz@gmail.com>
2020-11-03 16:38:21 +0100

    Episode routes

--- routes/episodeRoutes.js
+++ routes/episodeRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = new express.Router()
+const { SendData, BadRequest, ServerError} = require('../utils/responses')
+const { Helper } = require('../utils/helper')
 
 const Episodes = require('../data/episodes')
 
@@ -18,24 +20,68 @@ router.get('/', async (req, res) => {
 })
 
 /**
- Return: Episode by id
- URI: domain/episodes/{id}
- **/
-
-/**
  Return: All episodes by season
  URI: domain/episodes/season/{integer}
  **/
+router.get('/season/:season', async (req, res) => {
+    const { season } = req.params
+
+    try{
+        const data = await Episodes.filter( el => el.season === parseFloat(season))
+
+        if(data.length !== 0){
+            SendData(res, data)
+        } else {
+            BadRequest(res)
+        }
+
+    } catch {
+        ServerError(res)
+    }
+})
 
 /**
  Return: Episode by season and episode
- URI: domain/episodes/search?season={integer}&episode={integer}
+ URI: domain/episodes/season/{integer}/episode/{integer}
  **/
+router.get('/season/:season/episode/:episode', async (req, res) => {
+    const { season, episode } = req.params
+
+    try{
+        const data = await Episodes.filter( el => el.season === parseFloat(season) && el.episode === parseFloat(episode))
+
+        if(data.length !== 0){
+            SendData(res, data)
+        } else {
+            BadRequest(res)
+        }
+
+    } catch {
+        ServerError(res)
+    }
+})
 
 /**
  Return: Episode by title
- URI: domain/episodes/search?title={string}
+ URI: domain/episodes/title/{string}
  **/
+router.get('/title/:title', async (req, res) => {
+    const { title } = req.params
+    const query = Helper.Sanitize(title)
+
+    try{
+        const data = await Episodes.filter( el => Helper.Sanitize(el.title).includes(query))
+
+        if(data.length !== 0){
+            SendData(res, data)
+        } else {
+            BadRequest(res)
+        }
+
+    } catch {
+        ServerError(res)
+    }
+})
 
 
 module.exports = router
